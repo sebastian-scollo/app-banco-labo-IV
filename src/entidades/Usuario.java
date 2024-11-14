720ac6f0b6598ec1d344493352c09eac1e72804d
Martin Ibañez <martinibanez.89@gmail.com>
2024-11-14 00:14:19 -0300

    Corrijo clase Usuario

--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -1,69 +1,70 @@
 package entidades;
 
 public class Usuario {
-	private int IDUsuario;
-<<<<<<< HEAD
-	private String NombreUsuario;
-=======
-	private String nombreUsuario;
->>>>>>> 6ee51c63d636acecd76034c40579d86d02e722c1
-	private String password;
-	private int TipoUsuario; 
-	private Boolean estado;
-	
-	
-<<<<<<< HEAD
+    private int IDUsuario;
+    private String NombreUsuario;
+    private String password;
+    private int TipoUsuario; 
+    private Boolean estado;
 
-	public Usuario(int iDUsuario, String password, int tipoUsuario, Boolean estado) {
-=======
-	public Usuario(int iDUsuario,String nombreUsuario, String password, int tipoUsuario, Boolean estado) {
->>>>>>> 6ee51c63d636acecd76034c40579d86d02e722c1
-		
-		IDUsuario = iDUsuario;
-		this.nombreUsuario = nombreUsuario;
-		this.password = password;
-		TipoUsuario = tipoUsuario;
-		this.estado = estado;
-	}
-	public Usuario() {};
-	
-	public int getIDUsuario() {
-		return IDUsuario;
-	}
-	public void setIDUsuario(int iDUsuario) {
-		IDUsuario = iDUsuario;
-	}
-	public String getNombreUsuario() {
-		return nombreUsuario;
-	}
-	public void setNombreUsuario(String nombreUsuario) {
-		this.nombreUsuario = nombreUsuario;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public int getTipoUsuario() {
-		return TipoUsuario;
-	}
-	public void setTipoUsuario(int tipoUsuario) {
-		TipoUsuario = tipoUsuario;
-	}
-	public Boolean getEstado() {
-		return estado;
-	}
-	public void setEstado(Boolean estado) {
-		this.estado = estado;
-	}
-	
-	public String getNombreUsuario() {
-		return NombreUsuario;
-	}
-	public void setNombreUsuario(String nombreUsuario) {
-		NombreUsuario = nombreUsuario;
-	}
-	
+   
+    public Usuario(int IDUsuario, String NombreUsuario, String password, int TipoUsuario, Boolean estado) {
+        this.IDUsuario = IDUsuario;
+        this.NombreUsuario = NombreUsuario;
+        this.password = password;
+        this.TipoUsuario = TipoUsuario;
+        this.estado = estado;
+    }
 
+    
+    public Usuario(int IDUsuario, String password, int TipoUsuario, Boolean estado) {
+        this.IDUsuario = IDUsuario;
+        this.password = password;
+        this.TipoUsuario = TipoUsuario;
+        this.estado = estado;
+    }
+
+    // Constructor vacío
+    public Usuario() {}
+
+    // Getters y setters
+    public int getIDUsuario() {
+        return IDUsuario;
+    }
+
+    public void setIDUsuario(int IDUsuario) {
+        this.IDUsuario = IDUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return NombreUsuario;
+    }
+
+    public void setNombreUsuario(String NombreUsuario) {
+        this.NombreUsuario = NombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTipoUsuario() {
+        return TipoUsuario;
+    }
+
+    public void setTipoUsuario(int TipoUsuario) {
+        this.TipoUsuario = TipoUsuario;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
